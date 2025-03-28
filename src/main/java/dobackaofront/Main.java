3d45f8ce8f2d052896857c43fec3e04294dc9a68
hiroshionishi <hiroshiemarcia@gmail.com>
2025-03-28 15:19:25 -0300

    Aula11POO

---
+++ src/main/java/dobackaofront/Main.java
@@ -0,0 +1,7 @@
+package dobackaofront;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/dobackaofront/Pessoa.java
@@ -0,0 +1,22 @@
+package dobackaofront;
+
+public class Pessoa {
+    //estrutura
+
+    private String nome;
+    private String cpf;
+    private int idade;
+    private char sexo;
+    private String whatsapp;
+
+    public Pessoa(){
+        this.nome="";
+        this.cpf="";
+        this.idade=0;
+        this.sexo='m';
+        this.whatsapp="";
+
+    }
+
+}
+
